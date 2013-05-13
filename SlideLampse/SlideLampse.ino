f84124d5f123c318e4c9a1e62fbbdf1f8c3dec40
AlbertoC <alberto.cicchi@gmail.com>
2013-05-13 18:36:25 +0200

    Slide Lampse
    Correct version

--- SlideLampse/SlideLampse.ino
+++ SlideLampse/SlideLampse.ino
@@ -9,44 +9,34 @@
 int RECV_PIN = 11;
 
 IRrecv irrecv(RECV_PIN);
-
 decode_results results;
- 
 TKRelay relay(O0);
 
 void setup()
 {
   Serial.begin(9600);
   irrecv.enableIRIn(); // Start the receiver
-  
 }
 
 void loop() {
   if (irrecv.decode(&results)) {
     Serial.println(results.value, HEX);
     if((results.value == 0x80A ) || (results.value == 0xA)){
-
       OFF();
-
     } 
     else  if((results.value == 0x80B ) || (results.value == 0xB)){
-
       ON();
-
-    }else  if((results.value == 0x80C ) || (results.value == 0xC)){
-
+    }
+    else  if((results.value == 0x80C ) || (results.value == 0xC)){
       FLASH(1);
-
     }
     else  if((results.value == 0x80D ) || (results.value == 0xD)){
-
       FLASH(3);
-
     }
-    
     irrecv.resume(); // Receive the next value
   }
 }
+
 void FLASH(int seconds){
   int i;
   int loops;
